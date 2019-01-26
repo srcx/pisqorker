5111ccf8e94bb1d64f8418a409874ad7fa9b27c7
Štěpán Roh <srcx@users.noreply.github.com>
2019-01-27 00:33:34 +0100

    ComponentScan is not required.

--- src/main/java/cz/srnet/pisqorker/PisqorkerServer.java
+++ src/main/java/cz/srnet/pisqorker/PisqorkerServer.java
@@ -2,11 +2,8 @@ package cz.srnet.pisqorker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("cz.srnet.pisqorker.game")
-@ComponentScan("cz.srnet.pisqorker.rest")
 public class PisqorkerServer {
 
 	public static void main(String[] args) {
